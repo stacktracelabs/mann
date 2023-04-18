0c184ecba5779b35eab33798a14c3cae2eaa4b9f
Peter Stovka <stovka.peter@gmail.com>
2023-04-18 13:56:53 +0200

    wip

---
+++ src/Foo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace StackTrace\Mann;
+
+class Foo {
+
+    /**
+     * Always returns bar.
+     *
+     * @return string
+     */
+    public function bar(): string
+    {
+        return "bar";
+    }
+
+}
